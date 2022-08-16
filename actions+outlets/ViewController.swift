2ce0c6f251670d0a5a9dad4e17f32c9a70bce623
Megan <meganmarieesl@gmail.com>
2022-08-16 07:56:52 -1000

    I'm committing my actions and outlets changes here!

--- actions+outlets/ViewController.swift
+++ actions+outlets/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var nameText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButton(_ sender: Any) {
+        
+        if let newName = nameText.text {
+            nameLabel.text = newName
+        }
+    }
+    
 }
 
